ba5be826ccadb29d83e8b6c6cdf72192cc13e074
PelusinniDev <110785576+pelusinnidev@users.noreply.github.com>
2024-12-02 22:14:24 +0100

    Initial commit: Add basic SwiftUI application structure with ContentView, app entry point, and asset catalog.

---
+++ MyCosmo/MyCosmo/MyCosmoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCosmoApp.swift
+//  MyCosmo
+//
+//  Created by pol hernàndez bosch on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MyCosmoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
