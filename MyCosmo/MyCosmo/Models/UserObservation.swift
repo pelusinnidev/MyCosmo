3c6fef01d79b285925aff4d8b780f34752100717
PelusinniDev <110785576+pelusinnidev@users.noreply.github.com>
2025-01-07 12:21:11 +0100

    Refactor Planet model: Remove Planet.swift file and integrate Planet enum into UserObservation model for improved organization and maintainability.

--- MyCosmo/MyCosmo/Models/UserObservation.swift
+++ MyCosmo/MyCosmo/Models/UserObservation.swift
@@ -53,3 +53,19 @@ enum ImportanceLevel: String, Codable, CaseIterable {
     case high = "High"
     case critical = "Critical"
 } 
+
+enum Planet: String, Codable, CaseIterable {
+    case mercury = "Mercury"
+    case venus = "Venus"
+    case earth = "Earth"
+    case mars = "Mars"
+    case jupiter = "Jupiter"
+    case saturn = "Saturn"
+    case uranus = "Uranus"
+    case neptune = "Neptune"
+    case other = "Other"
+    
+    var defaultImage: Image {
+        Image(rawValue)
+    }
+} 
